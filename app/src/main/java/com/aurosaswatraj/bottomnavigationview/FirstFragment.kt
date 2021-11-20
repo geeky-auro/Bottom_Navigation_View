bddea134dada4b63693230d28ab51e860731a424
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2021-11-20 23:23:11 +0530

    Initial and Final commit by Auro

---
+++ app/src/main/java/com/aurosaswatraj/bottomnavigationview/FirstFragment.kt
@@ -0,0 +1,6 @@
+package com.aurosaswatraj.bottomnavigationview
+
+import androidx.fragment.app.Fragment
+
+class FirstFragment :Fragment(R.layout.fragment_first) {
+}
---
+++ app/src/main/java/com/aurosaswatraj/bottomnavigationview/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.aurosaswatraj.bottomnavigationview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val firstFragment=FirstFragment()
+        val secondFragment=SecondFragment()
+        val thirdFragment=ThirdFragment()
+        setCurrentFragment(firstFragment)
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.mihome->setCurrentFragment(firstFragment)
+                R.id.miMessages->setCurrentFragment(secondFragment)
+                R.id.miprofile->setCurrentFragment(thirdFragment)
+            }
+            true
+        }
+        bottomNavigationView.getOrCreateBadge(R.id.miMessages).apply {
+            number=19
+
+        }
+    }
+
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+}
